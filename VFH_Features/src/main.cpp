d48cd25afd76d7a746b01a82993fbe85861ea909
Katsileros <katsileros@gmail.com>
2015-03-20 19:59:41 +0200

    Add some comments

--- VFH_Features/src/main.cpp
+++ VFH_Features/src/main.cpp
@@ -1,10 +1,12 @@
 #include "create_features.h"
 
 int main(int argc, char **argv)
-{	
+{
+  /// NUmber of data to read	
   int num_train_data = 10;
-  
+  /// The data set folder
   std::string ss1 = "rgbd-dataset";
+  
   vfh_features *vfh = new vfh_features(ss1, num_train_data);
   vfh->buildTree();
   
